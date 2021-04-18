b2fff3f96b36e778e3c41e7f2eabe480a893d212
Hao Dong <1094998854@qq.com>
2021-04-18 20:33:06 +0300

    new

--- ekf_slam/octave/ekf_slam.m
+++ ekf_slam/octave/ekf_slam.m
@@ -45,8 +45,8 @@ mapSigma = INF*eye(2*N);
 sigma = [[robSigma robMapSigma];[robMapSigma' mapSigma]];
 
 % toogle the visualization type
-%showGui = true;  % show a window while the algorithm runs
-showGui = false; % plot to files instead
+showGui = true;  % show a window while the algorithm runs
+%showGui = false; % plot to files instead
 
 % Perform filter update for each odometry-observation pair read from the
 % data file.
